4c64add593e0b5685060da7ea7eb79d9c6e0755a
Viktor Vasilenko <nothardme@gmail.com>
2023-05-15 18:41:36 +0400

    Nodata page
    added page what appears when zero entities found in DB

--- flightdensity/flights/views.py
+++ flightdensity/flights/views.py
@@ -18,30 +18,38 @@ def flightsGraph(request):
         form = DatePicker({'date_wig': datetime.today()})
 
     all_flights = Flights.objects.filter(eta_date=day_for_seek)
-    depart_fights = all_flights.filter(is_depart=1)
-    arrive_flights = all_flights.filter(is_depart=0)
+    print(all_flights.count())
+    if all_flights.count() > 0:
+        depart_fights = all_flights.filter(is_depart=1)
+        arrive_flights = all_flights.filter(is_depart=0)
 
-    count_by_hours_all = []
-    count_by_hours_dep = []
-    count_by_hours_arr = []
-    for i in range(24):
-        count_by_hours_all.append(all_flights.filter(eta_time__hour=i).count())
-        count_by_hours_dep.append(depart_fights.filter(eta_time__hour=i).count())
-        count_by_hours_arr.append(arrive_flights.filter(eta_time__hour=i).count())
-    count_by_hours_all = json.dumps(count_by_hours_all)
-    count_by_hours_dep = json.dumps(count_by_hours_dep)
-    count_by_hours_arr = json.dumps(count_by_hours_arr)
+        count_by_hours_all = []
+        count_by_hours_dep = []
+        count_by_hours_arr = []
+        for i in range(24):
+            count_by_hours_all.append(all_flights.filter(eta_time__hour=i).count())
+            count_by_hours_dep.append(depart_fights.filter(eta_time__hour=i).count())
+            count_by_hours_arr.append(arrive_flights.filter(eta_time__hour=i).count())
+        count_by_hours_all = json.dumps(count_by_hours_all)
+        count_by_hours_dep = json.dumps(count_by_hours_dep)
+        count_by_hours_arr = json.dumps(count_by_hours_arr)
 
-    context = {
-        'title': "Самолеты в толмачево",
-        'data': {
-            'data1': count_by_hours_all,
-            'data2': count_by_hours_dep,
-            'data3': count_by_hours_arr
-        },
-        'form': form
-    }
-    return render(request, 'flights/index.html', context)
+        context = {
+            'title': "Самолеты в толмачево",
+            'data': {
+                'data1': count_by_hours_all,
+                'data2': count_by_hours_dep,
+                'data3': count_by_hours_arr
+            },
+            'form': form
+        }
+        return render(request, 'flights/index.html', context)
+    else:
+        context = {
+            'title': "Самолеты в толмачево",
+            'form': form
+        }
+        return render(request, 'flights/nodata.html', context)
 
 
 def download(request):
